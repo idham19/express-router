649fdb004d9c1a48b9b6a3413f8a455018c97062
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-09-24 22:04:25 -0400

    User router added with all method request

---
+++ routes/fruits.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const Fruits = require("../models/Fruit");
+const router = express.Router();
+
+router.get("/", async () => {});
+
+router.get("/:id", async () => {});
+
+router.post("/", async () => {});
+
+router.put("/", async () => {});
+
+router.delete("/", async () => {});
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const User = require("../models/User");
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const getAllUsers = await User.findAll();
+    res.json(getAllUsers);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const updateData = req.body;
+    const findUserById = await User.findByPk(userId);
+    res.json(findUserById);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const userData = req.body;
+    const newUser = await User.create(userData);
+    res.json(newUser);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const userData = req.body;
+    const findUser = await User.findByPk(userId);
+    await findUser.update(userData);
+    res.json(findUser);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const findUser = await User.findByPk(userId);
+    const findAllUsers = await User.findAll();
+    await findUser.destroy();
+    res.json(findAllUsers);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+module.exports = router;
