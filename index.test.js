cd5cd4074d16574efa49ff172dfb6540c6b810dd
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-09-24 22:43:34 -0400

    Get user by id test added to index.test.js

--- index.test.js
+++ index.test.js
@@ -10,4 +10,12 @@ describe("Users test", () => {
     expect(response.body[0].name).toEqual(allUsers[0].name);
     expect(response.body[0].age).toEqual(allUsers[0].age);
   });
+
+  test("Get user by id ", async () => {
+    const userId = 1;
+    const findUserById = await User.findByPk(userId);
+    const response = await request(app).get(`/users/${userId}`).expect(200);
+    expect(response.body.name).toEqual(findUserById.name);
+    expect(response.body.age).toEqual(findUserById.age);
+  });
 });
